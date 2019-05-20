d66ba1476dc75b6cfbc4de8e66b9803eb2e0c58b
Palash <maxzeroedge@gmail.com>
2019-05-20 22:38:10 +0530

    Updated pokemon model

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -34,9 +34,25 @@ class Pokemon{
 	}
 
 	Pokemon.fromJson(Map json):
+		id = json['id'] as int,
+		order = json['order'] as int,
+		base_experience = json['base_experience'] as int,
+		height = json['height'] as int,
+		width = json['width'] as int,
 		name = json['name'],
 		image = json['sprites'] != null ? json['sprites']['front_default'] : null,
-		url = json['url'];
+		url = json['url'],
+		location_area_encounters = json['location_area_encounters'],
+		is_default = json['is_default'] as bool,
+		abilities = (json['abilities'] as List).map((d)=>Ability.fromJson(d)),
+		forms = (json['forms'] as List).map((d)=>PokemonForm.fromJson(d)),
+		game_indices = (json['game_indices'] as List).map((d)=>GameIndices.fromJson(d)),
+		held_items = json['held_items'] as List,
+		moves = (json['moves'] as List).map((d)=>PokemonMove.fromJson(d)),
+		stats = (json['stats'] as List).map((d)=>PokemonStat.fromJson(d)),
+		types = (json['types'] as List).map((d)=>PokemonType.fromJson(d)),
+		species = NameUrlClass.fromJson(json['species']),
+		sprites = json['sprites'] as Map);
 
 	Map toJson(){
 		return {
